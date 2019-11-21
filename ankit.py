53fcb2011e889d30312d8107567093372237b2da
jbankit <ankitsinghrajput543@gmail.com>
2019-11-21 19:22:09 +0530

    write to speak AI computer assistant

---
+++ ankit.py
@@ -0,0 +1,8 @@
+import win32com.client as wincl
+speak = wincl.Dispatch("SAPI.SpVoice")
+speak.Speak("Hello and good morning everyone!")
+speak.Speak("I am Antonie 4.0! ")
+speak.Speak("created by bharat patidar. I warm heartly wecome you all to the launch of roboshala")
+speak.Speak("A journey towards innovation and creativity. ")
+speak.Speak("NOw I hand over the stage to Deevanshu and Twinkle!")
+speak.Speak("Hope you will enjoy the session")
